8f6def0bdd3a2f919b8cea0406c1bcf31e0ccfe7
Thirumugam S <alaissham@yahoo.com>
2017-06-30 14:48:06 +0530

    Add files via upload

---
+++ LocationHistory.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LocationHistory.aspx.cs" Inherits="MapsHistory.LocationHistory" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <asp:FileUpload id="FileUploadControl" runat="server" />
+                </td>
+                <td>
+                    <asp:Button runat="server" id="UploadButton" text="Upload" OnClick="UploadButton_Click"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblStatus" runat="server"></asp:Label>
+                </td>
+            </tr>
+    
+            </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ LocationHistory.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MapsHistory
+{
+    public partial class LocationHistory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+       
+         //   Response.Write(activity.getActivity());
+        }
+
+        public void CallActivitymethod()
+        {
+            Activity activity = new Activity();
+            activity.getActivity();
+            List<string> listOfActivites = activity.getActivityTypes();
+        }
+
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("~/Data/");
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists. Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+            string filename = Path.GetFileName(FileUploadControl.FileName);
+            FileUploadControl.SaveAs(folderPath + Path.GetFileName(FileUploadControl.FileName));
+            lblStatus.Text = Path.GetFileName(FileUploadControl.FileName) + " has been uploaded.";
+
+
+
+
+        }
+
+
+    }
+}
